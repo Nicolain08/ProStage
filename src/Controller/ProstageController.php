9d29273458f7d295e2ac9e150665415a8d1cf407
Nicolas Allain <nallain@iutbayonne.univ-pau.fr>
2020-12-03 12:07:02 +0100

    Fin du TP1

--- src/Controller/ProstageController.php
+++ src/Controller/ProstageController.php
@@ -9,12 +9,46 @@ use Symfony\Component\Routing\Annotation\Route;
 class ProstageController extends AbstractController
 {
     /**
-     * @Route("/prostage", name="prostage")
+     * @Route("/", name="accueil")
      */
-    public function index(): Response
+    public function afficherAccueil(): Response
     {
-        return $this->render('prostage/index.html.twig', [
-            'controller_name' => 'ProstageController',
+        return $this->render('prostage/baseTP1.html.twig', [
+            'controller_name' => 'Bienvenue sur la page d accueil de Prostages',
+			'page_title' => 'Prostage_Accueil',
+        ]);
+    }
+	
+	/**
+     * @Route("/entreprises", name="entreprises")
+     */
+    public function afficherEntreprises(): Response
+    {
+        return $this->render('prostage/baseTP1.html.twig', [
+            'controller_name' => 'Cette page affichera la liste des entreprises proposant un stage',
+			'page_title' => 'Prostage_Entreprises',
+        ]);
+    }
+	
+	/**
+     * @Route("/formations", name="formations")
+     */
+    public function afficherFormations(): Response
+    {
+        return $this->render('prostage/baseTP1.html.twig', [
+            'controller_name' => 'Cette page affichera la liste des formations de l IUT',
+			'page_title' => 'Prostage_Formations',
+        ]);
+    }
+	
+	/**
+     * @Route("/stages/{id}", name="stages")
+     */
+    public function afficherStages($id): Response
+    {
+        return $this->render('prostage/baseTP1.html.twig', [
+            'controller_name' => "Cette page affichera le descriptif du stage ayant pour identifiant $id",
+			'page_title' => 'Prostage_Stages',
         ]);
     }
 }
